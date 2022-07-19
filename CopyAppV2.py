91b213dfda8e61552197d1e3c5235dce9b12fffd
Maxis88 <104647331+Maxis88@users.noreply.github.com>
2022-07-19 21:09:47 +0200

    Update CopyAppV2.py

--- CopyAppV2.py
+++ CopyAppV2.py
@@ -9,7 +9,7 @@ import tkinter as tk
 window = tk.Tk()
 window.geometry("800x600")
 window.resizable(False, False)
-window.config(bg='dark grey')
+
 
 
 class Program():
@@ -43,42 +43,83 @@ class Program():
         tk.Button(self.ramka_glowna, text='Spakuj do ZIP', command=self.zip_it,
                   width=self.szerokosc_przyciskow).grid(column=1, row=3)
         self.ramka_glowna.pack(fill='both')
-        self.ramka_dolna = tk.Frame(window, width=700, height=300, background='dark grey')
-        self.ramka_dolna.pack(fill='both', pady=20)
+        self.ramka_dolna = tk.Frame(window, width=700)
+        self.ramka_dolna.pack(fill='both', pady=10)
         self.ramka_komunikatow = tk.Frame(window)
         self.aktualny_folder()
-        self.ramka_komunikatow.pack(fill='x', pady=20)
+        self.ramka_komunikatow.pack(fill='x', pady=10)
 
         tk.Label(self.ramka_dolna, text='Program służy do sekgregacji plików. \n'
-                                    'Upewnij się, że znajduje się w folderze, który chcesz uporządkować!\n'
-                                    'Posegregowane pliki będą znajdować się w folderach o '
+                                        'Instrukcja:\n'
+                                    '\t-Upewnij się, że znajduje się w folderze, który chcesz uporządkować!\n'
+                                    '\t-Posegregowane pliki będą znajdować się w folderach o '
                                     'nazwie rozszerzenia pliku...\n'
-                                    'Upewnij się, że znajdują się tu tylko pliki .exe tylko do instalacji.', justify='left', anchor='nw').pack(fill='both')
+                                    '\t-Sortowanie folderów polega na pogrupowaniu mniejszych folderów w ogólne.\n'
+                                    '\t-Upewnij się, że znajdują się tu tylko pliki .exe tylko do instalacji.\n'
+                                    '\t-Kolejność działań: od pierwszego przycisku do ostatniego lub "Sortuj automatycznie"\n'
+                                        '\t-Pakowanie do ZIP jest opcjonalne i nie jest automatycznie wykonywane przy '
+                                        'sortowaniu automatycznym\n\tjednak do jego wykonania potrzebujesz wykonać sortowanie folderów.'
+                                        '', justify='left', anchor='nw').pack(fill='both')
 
     def zip_it(self):
+        self.czyscRamki()
         foldery = os.listdir(self.sciezka)
         grupy = ['Archiwa', 'Dokumenty', 'Obrazy', 'Filmy', 'Instalki', 'Muzyka']
-        komunikaty='Rozpoczynam pracę...\n'
+
         spakowanych=0
+        znalezione = []
         for element in foldery:
             if os.path.isdir(element):
                 if element in grupy:
-                    komunikaty+=f'\t Znaleziono folder {element}\n\t Archiwizuję...\n'
-                    spakowanych += 1
-                    nazwa = element + '.zip'
+                    znalezione.append(element)
+        if len(znalezione) in range(2,5):
+            liczba = 'foldery'
+        elif len(znalezione)==1:
+            liczba='folder'
+        else:
+            liczba='folderów'
+        if len(znalezione)>0:
+            komunikaty=f'Znalazłem {len(znalezione)} {liczba}, które mogę zarchiwizować. Zaznacz, które Cię interesują:'
+            self.vars.clear()
 
-                    with ZipFile(nazwa, 'w') as file:
-                       subfoldery = os.listdir(element)
-                       for subfolder in subfoldery:
-                           pliki = os.listdir(f'{element}\{subfolder}')
-                           for plik in pliki:
+            tk.Label(self.ramka_dolna, text=komunikaty, justify='left', anchor='w').pack(fill='x')
+            for ele in znalezione:
+                var = tk.IntVar()
+                self.vars.append(var)
+                self.checkb=tk.Checkbutton(self.ramka_dolna, text=ele, variable=var, justify='left', name=str(ele.lower()))
+                self.checkb.pack(anchor='nw')
+                self.checkb.select()
+            tk.Button(self.ramka_dolna, text='Dalej',
+                      width=self.szerokosc_przyciskow,
+                      command=lambda: self.zip2(znalezione)).pack(anchor='w')
+        else:
+            komunikaty='Nie znalazłem żadnych folderów nadających się do zarchiwizowania...'
+            tk.Label(self.ramka_dolna, text=komunikaty, justify='left', anchor='w').pack(fill='x')
 
-                                adres = f'{element}\{subfolder}\{plik}'
-                                file.write(adres)
+    def zip2(self, lista):
+        self.czyscRamki()
 
+        znalezione = lista
+        komunikat='Zaczynam pracę:\n'
+        ukonczone=0
+        plikow=0
+        for index, x in enumerate(self.vars):
+            if x.get() == 1:
+                plikow+=1
+                element = znalezione[index]
+                nazwa = element + '.zip'
 
-        komunikaty+=f'Kończę pracę. Spakowanych plików: {spakowanych}'
-        tk.Label(self.ramka_komunikatow, text=komunikaty, justify='left', anchor='w').pack(fill='x')
+                with ZipFile(nazwa, 'w', ZIP_DEFLATED) as file:
+                    komunikat+=f'- Archiwizuję {element}\n'
+                    subfoldery = os.listdir(element)
+                    for subfolder in subfoldery:
+                        pliki = os.listdir(f'{element}\{subfolder}')
+                        for plik in pliki:
+                            adres = f'{element}\{subfolder}\{plik}'
+                            file.write(adres)
+                            ukonczone+=1
+        komunikat+=f'Zakończono. Spakowanych plików: {ukonczone} w {plikow} plikach ZIP'
+        tk.Label(self.ramka_komunikatow, text=komunikat, justify='left').pack(anchor='nw')
 
     def rob_wszystko(self):
         self.rozszerzenia.clear() # czyści zmienna
@@ -111,20 +152,39 @@ class Program():
                 licznik=0
         return tekst
 
+    def okienko(self, tekst, nazwa_okna='Błąd', ok_btn=True):
+        okienko = tk.Toplevel(window)
+        okienko.title(nazwa_okna)
+        if len(tekst)<40:
+            szerokosc=40
+        elif len(tekst)>40 and len(tekst)<100:
+            szerokosc=len(tekst)
+        else:
+            szerokosc=120
+
+        tk.Label(okienko, text=tekst, width=szerokosc, height=2).pack()
+        if ok_btn==True:
+            tk.Button(okienko, text='OK', command=lambda: okienko.destroy()).pack(pady=10)
+
     def aktualizuj_folder(self):
         aktualny = self.adres.get()
-        self.sciezka = aktualny
-        self.informacje.destroy()
-        self.czyscRamki()
-        self.aktualny_folder()
+        if aktualny != 'Tutaj wklej adres folderu' and os.path.isdir(aktualny):
+            self.sciezka = aktualny
+            self.informacje.destroy()
+            self.czyscRamki()
+            self.aktualny_folder()
+
+        else:
+            self.okienko('Nie podałeś adresu lub jest on nieprawidłowy.', 'Błąd', True)
+
 
     def aktualny_folder(self): # informacje o aktualnym folderze w ktorym znajduje sie program wykonujacy
         komunikaty='Dane o folderze:\n'
         komunikaty+=f'Aktualny folder: \n\t{self.sciezka} \n'
-        self.adres = tk.Entry(self.ramka_glowna, width=90)
+        self.adres = tk.Entry(self.ramka_glowna, width=75)
         self.adres.grid(column=0, row=5, columnspan=3, sticky='w')
-        self.adres.insert(0, self.sciezka)
-        zmien = tk.Button(self.ramka_glowna, text='Zmień', command=self.aktualizuj_folder)
+        self.adres.insert(0, 'Tutaj wklej adres folderu')
+        zmien = tk.Button(self.ramka_glowna, text='Zmień folder', command=self.aktualizuj_folder, width=self.szerokosc_przyciskow)
         zmien.grid(row=5, column=2, sticky='e')
 
         self.szukaj_rozszerzen()
@@ -162,6 +222,7 @@ class Program():
                     self.przeniesFolder('Instalki', element)
                 else:
                     pass
+
 
     def szukaj_rozszerzen(self):# tworzy liste rozszerzen
         # tworzy foldery glowne, z formetem plikow i porzadkuje zawartosc
@@ -213,7 +274,7 @@ class Program():
             potwierdz=tk.Button(self.ramka_komunikatow, text='Tak', command=lambda: self.segreguj_foldery(lista_folderow))
             potwierdz.pack()
         else:
-            tk.Label(self.ramka_komunikatow, text='Nie znaleziono folderów spełniających kryteria. Najpierw posortuj pliki.').pack(fill='x')
+            self.okienko('Nie znaleziono folderów spełniających kryteria. Najpierw posortuj pliki.', 'Błąd', True)
 
     def czyscRamki(self):
         for child in self.ramka_komunikatow.winfo_children():
@@ -307,6 +368,8 @@ class Program():
                 self.check.select()
 
             tk.Button(self.ramka_dolna, text='Dalej', command=self.sprawdzZaznaczone).pack()
+        else:
+            self.okienko('Nie znaleziono plików do sortowania.')
 
 
 Program()
